48a060950fee0fe657d1a4c9bccc9a033c15749f
Venkateshbodem <160826720+Venkateshbodem@users.noreply.github.com>
2024-11-14 11:09:39 +0530

    Delete gradle-typescript-plugin directory

--- gradle-typescript-plugin/src/main/groovy/at/irian/typescript/gradle/task/TypeScriptPluginTask.groovy
+++
@@ -1,9 +0,0 @@
-package at.irian.typescript.gradle.task
-
-import org.gradle.api.DefaultTask
-import at.irian.typescript.gradle.TypeScriptPluginExtension
-
-abstract class TypeScriptPluginTask extends DefaultTask {
-
-    abstract void setupInputsAndOutputs(TypeScriptPluginExtension extension);
-}
